870684c73700301874200b1eb5bab0e6c0714629
rodafa <rodrigo@vestapm.com>
2026-09-13 13:13:32 -0400

    Delete _salvage dead code

--- _salvage/integrations/boompay/__init__.py
+++
@@ -1,8 +0,0 @@
-from .client import BoompayClient
-from .services import ApplicationSyncService, ReportSyncService
-
-__all__ = [
-    "BoompayClient",
-    "ApplicationSyncService",
-    "ReportSyncService",
-]
--- _salvage/integrations/rentengine/__init__.py
+++
@@ -1,8 +0,0 @@
-from .client import RentEngineClient
-from .services import UnitSyncService, LeasingPerformanceSyncService
-
-__all__ = [
-    "RentEngineClient",
-    "UnitSyncService",
-    "LeasingPerformanceSyncService",
-]
